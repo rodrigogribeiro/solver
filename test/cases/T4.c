b0a103b6f0aaa3da7e63b84270e7c5b7a6820c6b
marcus <demaroar@gmail.com>
2016-03-07 14:18:52 -0300

    updating test case T4.c

--- test/cases/T4.c
+++ test/cases/T4.c
@@ -1,8 +1,15 @@
- float do_math(float arg1, int arg2) {
-        return arg2 / arg1;
-    }
+ //typedef float (*T)(float, int);
+// START CHALLENGE
 
- int call_a_func(T call_this) {
-        float output = call_this(5.5, 7);
-        return output;
-    }
+float do_math(float arg1, int arg2) {
+    return arg2 / arg1;
+}
+
+int call_a_func(T call_this) {
+    float output = call_this(5.5, 7);
+    return output;
+}
+
+int main() {
+  float final_result = call_a_func(&do_math);
+}
